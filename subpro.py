ddb961fa18714b15e36f31ddaef5839b0df6db6a
Guanglei Wang <wgleiuni@gmail.com>
2015-08-03 15:57:50 -0700

    Add progress bar.

--- subpro.py
+++ subpro.py
@@ -2,6 +2,7 @@
 import subprocess
 from subprocess import Popen, PIPE
 import numpy as np
+from progressbar import Bar, Percentage, ProgressBar
 
 h = 0.002
 h = h*np.pi
@@ -14,10 +15,10 @@ step = 11
 omega = np.linspace(0, 10, step)
 phi = np.linspace(0, 2, step)
 [omegav, phiv] = np.meshgrid(omega, phi)
+pbar = ProgressBar(widgets=[Percentage(),Bar()], maxval= step*step).start()
 for i in range(step):
     for j in range(step):
         tN = (i)*step+j+1
-        print tN
         cmd = ["./python_job.sh", "%d" % tN, "%f" % h, "%f" % sigma, "%f" % ri,
                "%f" % omegav[i, j], "%f" % phiv[i, j], "%d" % numdt]
         result = subprocess.Popen(cmd, stdout = subprocess.PIPE)
@@ -25,3 +26,6 @@ for i in range(step):
         out = result.stdout.read()
         outfile.write(out)
         outfile.close()
+        pbar.update(tN)
+
+pbar.finish()
